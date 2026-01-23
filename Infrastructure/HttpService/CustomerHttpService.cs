7e060db490e9fc459e80f87adb93f99f3c317cb7
ale.alencarr <alexandre-879@hotmail.com>
2026-01-23 11:14:08 -0300

    finalizacao quarta fase

--- Infrastructure/HttpService/CustomerHttpService.cs
+++ Infrastructure/HttpService/CustomerHttpService.cs
@@ -17,7 +17,7 @@ namespace TechChallenge_auth_function.Infrastructure.HttpService
 
         public async Task<Customer?> GetCustomerByCpf(string cpf)
         {
-            var result = await _httpClient.GetFromJsonAsync<CommandResult<Customer>>($"api/customers/{cpf}");
+            var result = await _httpClient.GetFromJsonAsync<CommandResult<Customer>>($"/customers/{cpf}");
 
             return result?.Data;
         }
